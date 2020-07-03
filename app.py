8731863de9e94e67b22c4f6d8007c184781846d1
pavzel <Pavel.V.Zelenin@gmail.com>
2020-07-03 16:30:55 +0000

    Tried to connect to MongoDB. A problem with access to data (commented lines 26-30 in app.py).

--- app.py
+++ app.py
@@ -4,16 +4,31 @@ from flask import Flask, render_template, redirect, request, url_for, session
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
+if os.path.exists('env.py'):
+    import env
+
 
 app = Flask(__name__)
 
-""" To be added. """
-# app.config['MONGO_DBNAME'] = ...
-# app.config['MONGO_URI'] = ...
-# app.secret_key = ...
+app.config['MONGO_DBNAME'] = 'letsplay'
+app.config['MONGO_URI'] = os.environ.get('MONGO_URI')
+app.secret_key = os.environ.get('SECRET')
+
+mongodb = PyMongo(app)
+
 
 @app.route('/')
 def index():
+    db_content = mongodb.db.users.find()
+    print("db_content: ", db_content)
+    """
+    # Failed attempt 1:
+    n_of_users = mongodb.db.users.count_documents({})
+    print("N of users: ", n_of_users)
+    #Failed attempt 2:
+    the_user = mongodb.db.users.find_one({"name": "Test User"})
+    print("the_user: ", the_user)
+    """
     return render_template('index.html')
 
 
