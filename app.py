7c3d034ccb9a08d244c30e5240b7f807dd414c18
pavzel <Pavel.V.Zelenin@gmail.com>
2020-07-06 08:12:04 +0000

    Added placeholder templates for home, activities_proposed, activities_done and games

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import os
-from flask import Flask, redirect, render_template, request, session, url_for, flash
+from flask import Flask, redirect, render_template, request, url_for
+from flask import session, flash
 from flask_pymongo import PyMongo
-from bson.objectid import ObjectId
+# from bson.objectid import ObjectId
 # Password and datetime look optional (Pasha)
-from werkzeug.security import generate_password_hash, check_password_hash
-from datetime import datetime
+# from werkzeug.security import generate_password_hash, check_password_hash
+# from datetime import datetime
 
 
 if os.path.exists('env.py'):
@@ -41,7 +42,45 @@ mongo = PyMongo(app)
 @app.route('/')
 def index():
     return render_template('public/index.html')
-    # return render_template('/public/login.html', register=mongo.db.register.find_one())
+    # return render_template('
+    # /public/login.html', register=mongo.db.register.find_one())
+
+
+"""
+Login page action. Method must be post.
+Find the given password and username and  if it matches then
+redirect to allrecipeslist but ifnot redirect to register and
+if password only incorrect then
+flash message to show that  password is incorrect
+"""
+
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    if request.method == 'POST':
+        login_user = mongo.db.register.find_one(
+            {'username': request.form['username']})
+        form = request.form
+        if login_user:
+            if(form['password'] == login_user['password']):
+                session['username'] = login_user['username']
+                session['status'] = login_user['status']
+                return redirect(url_for('home'))
+                # return redirect(url_for(
+                # '', register_id=login_user["_id"]))
+            else:  # and if password is not correct
+                flash("Incorrect password")
+        else:  # if user does not exist
+            flash("User does not exist")
+            return redirect(url_for('register'))
+    return render_template('/public/login.html')
+
+
+@app.route('/logout')
+def logout():
+    session.pop('user', None)
+    session.pop('status', None)
+    return redirect(url_for('index'))
 
 
 """
@@ -49,6 +88,7 @@ Register form action method must be post.
 Insert  new user in database using form on login page,
 then redirect user to allrecipeslist page.
 """
+
 
 @app.route('/register', methods=['POST', 'GET'])
 def register():
@@ -61,26 +101,24 @@ def register():
     return render_template('public/register.html')
 
 
-"""
-Login page action. Method must be post - find the given password and username and  if it matches then
-redirect to allrecipeslist but ifnot redirect to register and if password only incorrect flash message to show that  password is incorrect
-"""
-@app.route('/login', methods=["GET", "POST"])
-def login():
-    if request.method == 'POST':
-        login_user = mongo.db.register.find_one(
-            {'username': request.form['username']})
-        form = request.form
-        if login_user:
-            if(form["password"] == login_user["password"]):  # if password correct
-                session['username'] = login_user["username"]
-                return redirect(url_for('', register_id=login_user["_id"]))
-            else:  # and if password is not correct
-                flash("Incorrect password")
-        else:  # if user does not exist
-            flash("User does not exist")
-            return redirect(url_for('register'))
-    return render_template('/public/login.html')
+@app.route('/home')
+def home():
+    return render_template('public/home.html')
+
+
+@app.route('/activities_proposed')
+def activities_proposed():
+    return render_template('public/activities_proposed.html')
+
+
+@app.route('/activities_done')
+def activities_done():
+    return render_template('public/activities_done.html')
+
+
+@app.route('/games')
+def games():
+    return render_template('public/games.html')
 
 
 """
@@ -99,6 +137,7 @@ def index():
         print("key: ", key)
     return render_template('public/index.html', n_of_users=n_of_users)
 """
+
 
 if __name__ == '__main__':
     app.run(
