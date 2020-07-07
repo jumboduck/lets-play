5e75be67c7e87928ae7e2ba43ab0a8b779cae6a9
Andy-Osborne <ajaosborne@googlemail.com>
2020-07-07 19:39:07 +0100

    Remove jQuery and scaling of navbar links

--- static/js/main.js
+++ static/js/main.js
@@ -3,9 +3,5 @@ $("document").ready(function(){
     // When the mouse hovers over a nav link it applies the class to scale the link
     // and when it leaves, it removes class
 
-    $(".menu-link").on("mouseenter", event => {
-        $(event.currentTarget).addClass("menu-link-zoom");
-    }).on("mouseleave", event => {
-        $(event.currentTarget).removeClass("menu-link-zoom");
-    })
+
 })
